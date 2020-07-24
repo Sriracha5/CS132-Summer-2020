20fcbcc02751e12b849ff5d0e9682e3f457bf71c
Sriracha5 <64803838+Sriracha5@users.noreply.github.com>
2020-07-23 22:03:13 -0700

    HOP3
    Commit

---
+++ Increment-Decrement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+
+namespace Increment_Decrement
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int a = 0; a <= 15; a++)
+            {
+                Console.WriteLine(a); // execute command above 0-15
+            }    
+
+
+              
+        }
+    }
+}
