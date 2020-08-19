6d91b857eb2f882d90834397b5d638258a19088d
fernando mckenzie <fernando.a.mckenzie@live.com>
2020-08-19 16:47:17 -0700

    HOP submission

---
+++ Number_to_word/Number to word/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Number_to_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (num == 1)
+            {
+                Console.WriteLine("one");
+                if  (num == 2)
+                {
+
+                    Console.WriteLine("two");
+                    if (num == 3)
+                    { 
+                     Console.WriteLine("three")
+                    }
+                }
+            }
+        }
+    }
